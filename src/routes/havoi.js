7ba77a3d90100d4737bf225a8dd47d70e8458c83
bannu76 <vinay2bannu@gmail.com>
2024-07-16 21:25:56 +0530

    commit

--- src/routes/havoi.js
+++ src/routes/havoi.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const havoiController = require("../controllers/getdata");
+const getmainpageController = require("../controllers/getmainpage");
 
 router.get("/:id", havoiController.getdata);
+router.get("/", getmainpageController.getmainpage);
 
 module.exports = router;
